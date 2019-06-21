1b71800a1e955975aadf057facf67e5b79aafda4
connor <connor.jaksik@gmail.com>
2019-06-20 21:22:21 -0600

    added profile image mobile styling

--- src/components/about.js
+++ src/components/about.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import Image from "./image"
-import { Container, Row, Col } from 'reactstrap';
-import SkillContainer from "./skill-container"
+import { Row, Col } from "reactstrap";
 import ScrollAnimate from "../components/scroll-animate"
+import "./about.css"
 
 class AboutSection extends Component {
     render() {
@@ -10,6 +10,9 @@ class AboutSection extends Component {
             <div
                 id="about"
                 name="about"
+
+                // Styling for the about section
+                // Which is rendered on index.js
                 style={{
                     margin: `85vh auto 0`,
                     padding: `0px 1.0875rem 1.45rem`,
@@ -18,25 +21,22 @@ class AboutSection extends Component {
                     zIndex: `1`,
                     maxWidth: 960,
                     backgroundImage: `linear-gradient(rgba(0,0,0,0), rgb(246, 246, 246))`,
-
                 }}>
-                    
+
+                {/* A short bio */}
                 <Row justify="center">
                     <Col xs="12" md="8" style={{ background: `white` }}>
-                   
-                    <h1>Hi! I'm Connor</h1>
-
-                        <p>I'm a web developer and outdoorsman from Denver, Colorado. Over the past few years coding has become my medium for expressing my artistic side and anxious mind. I really enjoy being able to work with others and create useful, practical, and inovative things that can instantly be shared with anyone in the world. It's exciting that technology is consistently improving allowing us web developers to never get bored and create things today that may of been impossible yesterday. I look forward to meeting and colaborating with new people in the future to see what we can create together. Scroll down to check out my technical skills and some of my projects.</p>
+                        <h1>Hi! I'm Connor</h1>
+                        <p>I'm a web developer and outdoorsman from Denver, Colorado. Over the past few years coding has become my medium for expressing my artistic side. I really enjoy being able to work with others and create useful, practical, and inovative things that can instantly be shared with anyone in the world. It's exciting that technology is consistently improving allowing us web developers to never get bored and create things today that may of been impossible yesterday. I look forward to meeting and colaborating with new people in the future to see what we can create together. Scroll down to check out my technical skills and some of my projects.</p>
                         <p>Let's build something great together.</p>
                         <h5>Connor Jaksik</h5>
                     </Col>
-                    <Col xs="10" md="4" style={{ marginBottom: `1.45rem`, maxWidth: `300px`, margin: `0 auto`, height: `400px` }}>
-                        <ScrollAnimate
-                    speed={8}
-                    >
-                        <Image />
 
-                    </ScrollAnimate>
+                    {/* A profile image */}
+                    <Col xs="10" md="4" className="pro-img">
+                        <ScrollAnimate speed={8}>
+                            <Image />
+                        </ScrollAnimate>
                     </Col>
                 </Row>
             </div>
