062c38886086338443a7dbf2a7f4da44e0588af4
connor <connor.jaksik@gmail.com>
2019-06-23 13:23:34 -0600

    added events div, not rendering yet

--- src/components/about-section.js
+++ src/components/about-section.js
@@ -12,43 +12,53 @@ class AboutSection extends Component {
         return (
             <div id="about-background">
                 <Section>
-                <div id="about" name="about">
- 
-                    {/* A short bio */}
-                    <Row>
-                        <Col xs="12" md="8" style={{ background: `#f6f6f6`, borderRadius: `25px` }}>
-                            <h1>Hi! I'm Connor</h1>
-                            <p>I'm a web developer and outdoorsman from Denver, Colorado. Over the past few years coding has become my medium for expressing my artistic side. I really enjoy being able to work with others and create useful, practical, and inovative things that can instantly be shared with anyone in the world. It's exciting that technology is consistently improving allowing us web developers to never get bored and create things today that may of been impossible yesterday. I look forward to meeting and colaborating with new people in the future to see what we can create together. Scroll down to check out my technical skills and some of my projects.</p>
-                            <p>Let's build something great together.</p>
-                            <h5>Connor Jaksik</h5>
-                        </Col>
+                    <div id="about" name="about">
 
-                        {/* A profile image */}
-                        <Col xs="12" md="4">
-                            <div className="pro-img">
-                                <ScrollAnimate speed={8}>
-                                    <Image />
-                                </ScrollAnimate>
+                        {/* A short bio */}
+                        <Row>
+                            <Col xs="12" md="8" style={{ background: `#f6f6f6`, borderRadius: `25px` }}>
+                                <h1>Hi! I'm Connor</h1>
+                                <p>I'm a web developer and outdoorsman from Denver, Colorado. Over the past few years coding has become my medium for expressing my artistic side. I really enjoy being able to work with others and create useful, practical, and inovative things that can instantly be shared with anyone in the world. It's exciting that technology is consistently improving allowing us web developers to never get bored and create things today that may of been impossible yesterday. I look forward to meeting and colaborating with new people in the future to see what we can create together. Scroll down to check out my technical skills and some of my projects.</p>
+                                <p>Let's build something great together.</p>
+                                <h5>Connor Jaksik</h5>
+                            </Col>
 
+                            {/* A profile image */}
+                            <Col xs="12" md="4">
+                                <div className="pro-img">
+                                    <ScrollAnimate speed={8}>
+                                        <Image />
+                                    </ScrollAnimate>
+
+                                </div>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <div id="button-container">
+                                <a href={resume} download >
+                                    <Button color="info" id="resume-button">Resume</Button>
+                                </a>
                             </div>
-                        </Col>
-                    </Row>
+                        </Row>
 
-                    <Row>
-                        <div id="button-container">
-                            <a href={resume} download >
-                                <Button color="info" id="resume-button">Resume</Button>
-                            </a>
-                        </div>
-                    </Row>
-
-                    <Row>
-                        <Col xs="12">
-                            <SkillSection />
-                        </Col>
-                    </Row>
-                </div>
-            </Section>
+                        <Row>
+                            <Col xs="12">
+                                <SkillSection />
+                            </Col>
+                        </Row>
+                    </div>
+                </Section>
+                {/* <Section>
+                    <div id="events" name="events" style={{ background: `#f6f6f6`, borderRadius: `25px` }}>
+                        <h2>Events</h2>
+                            <ul>
+                                <li>
+                                    08/15/2019 - AWS Solutions Architect Certification Exam
+                                </li>
+                            </ul>
+                    </div>
+                </Section> */}
             </div>
         )
     }
--- src/components/skill-section.js
+++ src/components/skill-section.js
@@ -1,32 +1,39 @@
 import React from "react"
 import { Container, Row, Col } from 'reactstrap'
 import data from "../data/skill-data.json"
+import "./skills.css"
 
-const skillContainer = () => (
+const skillContainer = () => {
+    let counter = 0;
+    return (
     <div className="skill-section">
-        <Container style={{ background: `rgba(255, 255, 255, 0.9)`, borderRadius: `25px`, marginBottom: `25px` }}>
+        <Container style={{ background: `rgba(246, 246, 246, 0.9)`, borderRadius: `25px`, marginBottom: `25px` }}>
 
             <h2>Technical Skills</h2>
-            {data.map(category => (
-                <>
-                    <Row>
-                        <Col xs="6" style={{ borderRight: `2px solid black` }}>
-                            <h5 style={{ margin: `14% auto`, textAlign: `center` }}>{category.title}</h5>
-                        </Col>
-                        <Col xs="6">
-                            <ul>
-                                {category.items.map(item => (
-                                    <li>{item}</li>
-                                ))}
-                            </ul>
-                        </Col>
-                    </Row>
-                    <div style={{ margin: `25px 0px`, borderBottom: `2px solid black` }}></div>
-                </>
-            ))}
+            {data.map((category, index) => {
+                console.log(data.length)
+                return (
+                    <>
+                        <Row>
+                            <Col xs="6" style={{ borderRight: `2px solid black` }}>
+                                <h5 style={{ margin: `14% auto`, textAlign: `center` }}>{category.title}</h5>
+                            </Col>
+                            <Col xs="6">
+                                <ul>
+                                    {category.items.map(item => (
+                                        <li>{item}</li>
+                                    ))}
+                                </ul>
+                            </Col>
+                        </Row>
+                        <div className={((index + 1) < data.length ? "bottom-border" : "")} style={{ margin: `25px 0px`}}></div>
+                    </>
+                )
+            }
+            )}
 
         </Container>
     </div>
-)
+)}
 
 export default skillContainer
