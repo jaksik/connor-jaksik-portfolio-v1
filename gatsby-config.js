30249b8bc284abddba4785b86ff8c2c0609200b4
connor <connor.jaksik@gmail.com>
2019-06-25 16:27:04 -0600

    updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Connor Jaksik`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Connor Jaksik's official portfolio.`,
+    author: `@jaksik`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
