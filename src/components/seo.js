9ebc379ed3638ae3b6d7958c4af0abfe76f10a60
connor <connor.jaksik@gmail.com>
2019-06-25 16:19:17 -0600

    added website image thumbnail

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import connor from "../images/connor.jpg"
 
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(
@@ -46,6 +47,10 @@ function SEO({ description, lang, meta, keywords, title }) {
         {
           property: `og:description`,
           content: metaDescription,
+        },
+        {
+          property: `og:image`,
+          content: connor,
         },
         {
           property: `og:type`,
