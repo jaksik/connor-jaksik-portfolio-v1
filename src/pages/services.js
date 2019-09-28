5e9f21a3730f32ae81230feb89b5ea788cd57754
connor <connor.jaksik@gmail.com>
2019-09-28 07:29:58 -0600

    added services data json

--- src/pages/services.js
+++ src/pages/services.js
@@ -4,7 +4,7 @@ import { Row, Col } from "reactstrap"
 import Layout from "../components/layout"
 import PageCoverImg from "../components/page-cover"
 import SEO from "../components/seo"
-
+import serviceData from "../data/service-data"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.css'
 
@@ -18,56 +18,28 @@ const ServicesPage = ({ data }) => {
         
        <PageCoverImg image={data.file.childImageSharp.fluid} title="What We Do"/>
 
-
-        <div className="slant" style={{background:`white`, color: `black`}}>
-          <Row className="no-gutters">
-            <Col xs="12">
-              <h2>Capabilities and Services</h2>
-            </Col>
-            <Col xs="12">
-              <p style={{textAlign:`center`}}>First thing we do is get to know your business, then we get to work.</p>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col xs="12">
-              <h2>Design</h2>
-            </Col>
-          </Row>
-
-          <ul>
-            <li>Wireframing</li>
-            <li>UI & UX</li>
-            <li>Content Creation</li>
-            <li>Email Design</li>
-          </ul>
-
-          <Row>
-            <Col xs="12">
-              <h2>Marketing</h2>
-            </Col>
-          </Row>
-          
-          <ul>
-            <li>SEO</li>
-            <li>Content Creation</li>
-            <li>Email Marketing</li>
-            <li>Email Design</li>
-          </ul>
-
-          <Row>
-            <Col xs="12">
-              <h2>Technology</h2>
-            </Col>
-          </Row>
-          
-          <ul>
-            <li>Front & Back-end</li>
-            <li>Integrations</li>
-            <li>Hosting</li>
-            <li>Mobile Apps</li>
-          </ul>
-        </div>
+        {servicesData.map((service, index) => {
+          return (
+            <>
+              <Row className="no-gutters justify-content-center">
+                <Col>
+                  <h2>{service.category}</h2>
+                </Col>
+              </Row>
+              {/* <Row>
+               <ul>
+               {service.services.map((item, i) => {
+                  return (
+                    <Col xs="12" sm="6">
+                        <li>{item}</li>
+                    </Col>
+                  )
+                })}
+               </ul>
+              </Row> */}
+            </>
+          )
+        })}
     </Layout>
   )
 }
